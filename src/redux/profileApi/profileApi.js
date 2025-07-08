d55b2a03469f278106bc26bd3939d8acffc1ed41
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-08 18:55:17 +0600

    Add profileApi with endpoints for user following, follower, and profile updates

---
+++ src/redux/profileApi/profileApi.js
@@ -0,0 +1,86 @@
+import { api } from "../api/baseApi";
+
+export const profileApi = api.injectEndpoints({
+  overrideExisting: true, //Prevents duplicate endpoint errors
+  endpoints: (builder) => ({
+    // Login
+    getUserFollowing: builder.query({
+      query: ({ user_id }) => ({
+        url: "/get-following", // This should match your Laravel route
+        params: {
+          user_id,
+        },
+      }),
+      providesTags: ["profile"],
+    }),
+    getUserFollowe: builder.query({
+      query: ({ user_id }) => ({
+        url: "/get-follower", // This should match your Laravel route
+        params: {
+          user_id,
+        },
+      }),
+      providesTags: ["profile"],
+    }),
+
+    updateUserProfile: builder.mutation({
+      query: (formData) => ({
+        url: "/update-user-profile",
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formData,
+      }),
+      invalidatesTags: ["profile"],
+    }),
+    userBlockProfile: builder.mutation({
+      query: ({ blocked_id }) => ({
+        url: "/user-block",
+        method: "POST",
+        params: {
+          blocked_id,
+        },
+      }),
+      invalidatesTags: ["profile"],
+    }),
+    userReportPost: builder.mutation({
+      query: ({ reported_id, content }) => ({
+        url: "/user-report",
+        method: "POST",
+        params: {
+          reported_id,
+          content,
+        },
+      }),
+      invalidatesTags: ["profile"],
+    }),
+
+    // Login
+    getUserAllFollowing: builder.query({
+      query: () => ({
+        url: "/get-following", // This should match your Laravel route
+        method: "GET",
+      }),
+      providesTags: ["profile"],
+    }),
+    getUserAllFollowe: builder.query({
+      query: () => ({
+        url: "/get-follower", // This should match your Laravel route
+        method: "GET",
+      }),
+      providesTags: ["profile"],
+    }),
+  }),
+});
+
+// Export hooks
+export const {
+  useGetUserFollowingQuery,
+  useGetUserFolloweQuery,
+  useGetUserAllFollowingQuery,
+  useGetUserAllFolloweQuery,
+  useUpdateUserProfileMutation,
+  useUserBlockProfileMutation,
+  useUserReportPostMutation,
+} = profileApi;
