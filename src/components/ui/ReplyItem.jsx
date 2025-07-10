c0581567da8486e1149d4fd036d5ad244763844f
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-10 16:50:11 +0600

    removerd the static data Reply Item  page

--- src/components/ui/ReplyItem.jsx
+++ src/components/ui/ReplyItem.jsx
@@ -1,7 +1,9 @@
 import tw from "@/src/lib/tailwind";
+import { makeImage } from "@/src/redux/api/baseApi";
 import { Image, Text, View } from "react-native";
+import { DEFAULT_AVATAR } from "./CommentItem";
 
-const DEFAULT_AVATAR = "https://randomuser.me/api/portraits/men/1.jpg";
+// const DEFAULT_AVATAR = "https://randomuser.me/api/portraits/men/1.jpg";
 
 const formatTime = (isoString) =>
   new Date(isoString).toLocaleTimeString([], {
@@ -10,12 +12,13 @@ const formatTime = (isoString) =>
   });
 
 const ReplyItem = ({ reply }) => {
+  console.log(reply);
+
   return (
     <View style={tw`flex-row gap-3 py-2 pl-3`}>
-      <View style={tw`absolute -left-8 h-0.6 w-16 bg-primary top-6`} />
       <Image
-        source={{ uri: DEFAULT_AVATAR }}
-        style={tw`w-8 h-8 rounded-full mt-1 z-10`}
+        source={{ uri: makeImage(reply?.avatar) || DEFAULT_AVATAR }}
+        style={tw`w-8 h-8 rounded-full mt-1 `}
       />
       <View style={tw`flex-1 bg-gray-50 rounded-lg p-3`}>
         <View style={tw`flex-row justify-between items-start`}>
